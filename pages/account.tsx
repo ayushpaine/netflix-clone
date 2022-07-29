6b0f05ed00762c5254a5d95e18c1200fb068a6cd
ayushpaine <ayushmt701@gmail.com>
2022-07-29 10:23:34 +0530

    sign out added on accounts page and styling added

--- pages/account.tsx
+++ pages/account.tsx
@@ -6,14 +6,16 @@ import useAuth from "../hooks/useAuth";
 import { getProducts, Product } from "@stripe/firestore-stripe-payments";
 import payments from "../lib/stripe";
 import { GetStaticProps } from "next/types";
+import Membership from "../components/Membership";
 
 interface Props {
   products: Product[];
 }
 
 const Account = ({ products }: Props) => {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
   const subscription = useSubs(user);
+  console.log(products);
 
   return (
     <div>
@@ -21,7 +23,6 @@ const Account = ({ products }: Props) => {
         <title>Account Settings</title>
         <link rel="icon" href="/netflix.svg" />
       </Head>
-
       <header className={`bg-[#141414]`}>
         <Link href="/">
           <img
@@ -39,8 +40,8 @@ const Account = ({ products }: Props) => {
           />
         </Link>
       </header>
-      <main className="pt-24">
-        <div>
+      <main className="pt-24 mx-auto max-w-6xl px-5 pb-12 transition-all md:px-10">
+        <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
           <h1 className="text-3xl md:text-4xl text-semibold">Account</h1>
           <div className="-ml-0.5 flex items-center gap-x-1.5">
             <img src="https://rb.gy/4vfk4r" alt="" className="h-7 w-7" />
@@ -50,11 +51,29 @@ const Account = ({ products }: Props) => {
           </div>
         </div>
 
-        {/* <Membership /> */}
+        <Membership />
 
-        <div>
+        <div className="account-div">
           <h4>Plan Details</h4>
-          <div>{}</div>
+          <div className="col-span-2 font-medium">
+            {
+              products.filter(
+                (product) => product.id === subscription?.product
+              )[0]?.name
+            }
+          </div>
+          <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
+            Change Plan
+          </p>
+        </div>
+        <div className="account-div">
+          <h4 className="text-lg text-[gray]">Settings</h4>
+          <p
+            className="col-span-3 cursor-pointer text-blue-500 hover:underline"
+            onClick={logOut}
+          >
+            Sign out of all devices
+          </p>
         </div>
       </main>
     </div>
